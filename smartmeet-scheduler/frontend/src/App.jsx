d7b1df3f8a8a242ed40e0b0a98277a6710a77205
tanmayaurl <tanmayadas815@gmail.com>
2025-10-31 19:49:08 +0530

    ch4

--- smartmeet-scheduler/frontend/src/App.jsx
+++ smartmeet-scheduler/frontend/src/App.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 import ReportsPage from "./pages/reportspage";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gradient-to-br from-[#0f172a] via-[#1e1b4b] to-[#312e81] text-gray-100">
+      <div className="min-h-screen bg-linear-to-br from-[#0f172a] via-[#1e1b4b] to-[#312e81] text-gray-100">
         <Navbar />
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/reports" element={<ReportsPage />} />
         </Routes>
